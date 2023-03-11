56e1187a7a2fceb9d36aa2b10f9a676d451dc5cc
jitendra <jitsingh182@gmail.com>
2023-03-11 10:20:11 +0530

    First Commit

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../src/loading.css'
+
+function Loading() {
+    return (
+        <div class="loader">
+            <span>Loading...</span>
+            <span>Loading...</span>
+        </div>
+    )
+}
+
+export default Loading
